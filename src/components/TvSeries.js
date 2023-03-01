29301ff49a9ea0df6d7f55dec061109ed195c874
Trust Okerezi <trustokerezi42@gmail.com>
2023-03-01 17:24:45 +0100

    tv series page created

--- src/components/TvSeries.js
+++ src/components/TvSeries.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { useGetLatestTvSeriesQuery } from "../services/Tv";
+import MovieCard from "./MovieCard";
 
 const TvSeries = () => {
   const { data, error, isLoading } = useGetLatestTvSeriesQuery();
-  console.log(data);
+  console.log(data?.results);
   try {
     return (
       <div className="grid grid-cols-2 md:grid-cols-4 gap-3 h-screen overflow-y-scroll">
-        Hello
+        {error ? (
+          <div></div>
+        ) : isLoading ? (
+          <div></div>
+        ) : data ? (
+          data.results?.map((tv, i) => (
+            <MovieCard
+              title={tv.name}
+              poster={tv.poster_path}
+              v_a={tv.vote_average}
+              release_date={tv.first_air_date}
+            />
+          ))
+        ) : null}
       </div>
     );
   } catch (error) {
